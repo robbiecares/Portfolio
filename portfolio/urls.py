2ea141d53702cbef3e9881d664ac3a10671357ce
robbiecares <robbiecares@gmail.com>
2021-03-22 16:48:04 +0100

    barebones is up

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'portfolio'
+
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('tutorials/', views.tutorials, name='tutorials'),
+    path('journal/', views.journal, name='journal'),
 
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,47 @@ from django.shortcuts import render
 def index(request):
     template = 'portfolio/index.html'
 
-    return render(request, template, {})
+    context = {
+        'title': 'Home'
+    }
+
+    return render(request, template, context)
+
+
+def about(request):
+    template = 'portfolio/about.html'
+
+    context = {
+        'title': 'About'
+    }
+
+    return render(request, template, context)
+
+
+def projects(request):
+    template = 'portfolio/projects.html'
+
+    context = {
+        'title': 'Projects'
+    }
+
+    return render(request, template, context)
+
+
+def tutorials(request):
+    template = 'portfolio/tutorials.html'
+
+    context = {
+        'title': 'Tutorials'
+    }
+
+    return render(request, template, context)
+
+def journal(request):
+    template = 'portfolio/journal.html'
+
+    context = {
+        'title': 'Journal'
+    }
+
+    return render(request, template, context)
